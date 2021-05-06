34547102623c7289ce4b648915c022bd9bb2929c
Devi <666191@M-AMBIN03725s-MacBook-Pro.local>
2021-05-06 22:59:01 +0530

    finall commit

---
+++ Luxpmsoft_Test/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Luxpmsoft_Test
+//
+//  Created by Devi, Kantlala Sushmitha (Cognizant) on 05/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var LoginButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        LoginButton.layer.cornerRadius = 5
+        
+        // Do any additional setup after loading the view.
+    }
+    
+  
+
+
+}
+
